ca581d84686aa603f01553a671b007a2b58ec082
AJ Tatum <andrew.j.tatum@gmail.com>
2019-11-19 23:57:00 -0500

    Added an overload for GetRawBodyStringAsync to stop breaking changes.

--- BabouExtensions/HttpRequestExtensions.cs
+++ BabouExtensions/HttpRequestExtensions.cs
@@ -10,8 +10,24 @@ namespace BabouExtensions
     /// </summary>
     public static class HttpRequestExtensions
     {
+
         /// <summary>
         /// Retrieve the raw body as a string from the Request.Body stream
+        /// </summary>
+        /// <param name="request">Request instance to apply to</param>
+        /// <param name="encoding">Optional - Encoding, defaults to UTF8</param>
+        /// <returns></returns>
+        public static async Task<string> GetRawBodyStringAsync(this HttpRequest request, Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            using var reader = new StreamReader(request.Body, encoding);
+            return await reader.ReadToEndAsync();
+        }
+
+        /// <summary>
+        /// Retrieve the raw body as a string from the Request.Body stream. Call Request.EnableBuffering() before using this method.
         /// </summary>
         /// <param name="request">Request instance to apply to</param>
         /// <param name="encoding">Optional - Encoding, defaults to UTF8</param>
