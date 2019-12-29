143d329cd04c31976b1f241280e0896e52699acb
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-28 22:07:54 -0500

    Added to do some testing

---
+++ BabouExtensions.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BabouExtensions;
+
+namespace BabouExtensions.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var convertToCsv1 = "Hello,true,1,there,here\r\nlinebreak,nobreak,Hello".ConvertToCsv(',', ',', null, true, " ", true);
+            var convertToCsv2 = "Hello,true,1,there,here\r\nlinebreak,nobreak,Hello".ConvertToCsv(',', ',', null, false, null, false);
+            var convertToCsv3 = "Hello,true,1,there,here\r\nlinebreak,nobreak,Hello".ConvertToCsv(',', ',', true, false, null, false);
+            var convertToCsv4 = "Hello,true,1,there,here\r\nlinebreak,nobreak,Hello".ConvertToCsv(',', ',', false, false, null, false);
+
+
+            System.Console.WriteLine(convertToCsv1);
+            System.Console.WriteLine(Environment.NewLine);
+            System.Console.WriteLine(convertToCsv2);
+            System.Console.WriteLine(Environment.NewLine);
+            System.Console.WriteLine(convertToCsv3);
+            System.Console.WriteLine(Environment.NewLine);
+            System.Console.WriteLine(convertToCsv4);
+            System.Console.ReadLine();
+        }
+    }
+}
