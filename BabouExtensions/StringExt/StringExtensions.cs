2325445d0043eefc358e724221c11fa7f1e92c8e
AJ Tatum <ajtatum@pm.me>
2021-01-19 15:45:43 -0500

    Removed unnecessary using

--- BabouExtensions/StringExt/StringExtensions.cs
+++ BabouExtensions/StringExt/StringExtensions.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
